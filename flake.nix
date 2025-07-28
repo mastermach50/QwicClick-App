9c4a0ffa451bc96d56dc00de8824a2b2f4886a74
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2025-07-28 23:41:42 +0530

    Configured building using nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,40 @@
 {
-  description = "flake.nix for QwicClick-App Environment";
+  description = "flake.nix for QwicClick-App";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      PORT = "3310";
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
+      packages.${system}.default = pkgs.buildNpmPackage {
+        pname = "qwic-click-app";
+        version = "0.0.1";
+        description = "The QwicClick Web App";
+
+        src = ./.;
+        npmDepsHash = "sha256-sehJGc8cy3ctORl1gZGyYEUgqWFS2xqlKqUq3LuOIAk=";
+
+        inherit PORT;
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cat > $out/bin/qwic-click-app << EOF
+          #!${pkgs.bash}/bin/bash
+          ${pkgs.nodejs}/bin/node build
+          EOF
+          chmod +x $out/bin/qwic-click-app
+        '';
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
         name = "QwC";
         buildInputs = with pkgs; [
           nodejs
         ];
-        PORT = "3310";
+        inherit PORT;
       };
     };
 }
